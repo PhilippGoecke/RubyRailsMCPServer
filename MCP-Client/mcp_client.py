7150b1e96e779fc6a9d6a919596ed462b0c46dde
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-11-22 23:26:40 +0100

    Update MCP server URL and modify agent prompt

--- MCP-Client/mcp_client.py
+++ MCP-Client/mcp_client.py
@@ -4,13 +4,12 @@ from langchain_ollama.chat_models import ChatOllama
 from mcp_use import MCPAgent, MCPClient
 
 # MCP server config
-AUTH_TOKEN="secret-token"
 CONFIG = {
   "mcpServers": {
     "http_server": {
-      "url": "https://localhost:3000",
+      "url": "http://host.containers.internal:3000/mcp",
       "headers": {
-        "Authorization": "Bearer ${AUTH_TOKEN}"
+        "Authorization": "Bearer secret-token"
       }
     }
   }
@@ -18,13 +17,13 @@ CONFIG = {
 
 async def main():
     client = MCPClient.from_dict(CONFIG)
-    llm = ChatOllama(model="qwen3:8b", base_url="http://localhost:11434")
+    llm = ChatOllama(model="qwen3:8b", base_url="http://host.containers.internal:11434")
 
     # LLM Agent of the MCPClient
     agent = MCPAgent(llm=llm, client=client, max_steps=20)
 
     # Prompt the Agent
-    result = await agent.run("Tell me my open Todo's")
+    result = await agent.run("Tell me all Todo's")
     print("\n LLM Reply:", result)
 
     # Close all MCPClient sessions
