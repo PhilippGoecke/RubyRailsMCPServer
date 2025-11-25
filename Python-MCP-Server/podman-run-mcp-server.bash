c642a07bce36bb4952f778a0cefd968393883069
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-11-25 23:11:07 +0100

    Update podman run command to use --publish flag

--- Python-MCP-Server/podman-run-mcp-server.bash
+++ Python-MCP-Server/podman-run-mcp-server.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm -f Containerfile -t pythonmcpserver:demo .
-podman run --interactive --tty -p 3000:3000 pythonmcpserver:demo
+podman run --interactive --tty --publish 3000:3000 pythonmcpserver:demo
 echo "browse http://localhost:3000/health"
