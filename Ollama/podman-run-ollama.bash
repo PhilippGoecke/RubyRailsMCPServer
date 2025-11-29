79aa6b530dd7f0018f6009d0c6825857631fc5e1
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-11-29 11:40:23 +0100

    Fix container name in podman run command

--- Ollama/podman-run-ollama.bash
+++ Ollama/podman-run-ollama.bash
@@ -1,2 +1,2 @@
-podman run --detach --gpus=all -v ollama:/root/.ollama --publish 11434:11434 --env OLLAMA_API_KEY=secret-key --name docker.io/ollama ollama/ollama
+podman run --detach --gpus=all -v ollama:/root/.ollama --publish 11434:11434 --env OLLAMA_API_KEY=secret-key --name ollama docker.io/ollama/ollama
 podman exec -it ollama ollama pull qwen3:8b
