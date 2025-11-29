224858aea595e96952c0a5e5892a0d50e300b9b6
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-11-29 19:47:17 +0100

    Comment out podman run command
    Commented out the podman run command for ollama.

--- Ollama/podman-run-ollama.bash
+++ Ollama/podman-run-ollama.bash
@@ -1,3 +1,4 @@
 podman run --detach --gpus=all -v ollama:/root/.ollama --publish 11434:11434 --env OLLAMA_API_KEY=secret-key --name ollama docker.io/ollama/ollama
 podman exec -it ollama ollama pull qwen3:8b
+#podman run --detach ollama
 podman logs --follow ollama
