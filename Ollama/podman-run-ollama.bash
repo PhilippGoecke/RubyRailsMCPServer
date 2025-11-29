f09056e96abdd592bbc63e849d22a0359033938b
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-11-29 19:25:53 +0100

    Add logging command to podman-run-ollama script

--- Ollama/podman-run-ollama.bash
+++ Ollama/podman-run-ollama.bash
@@ -1,2 +1,3 @@
 podman run --detach --gpus=all -v ollama:/root/.ollama --publish 11434:11434 --env OLLAMA_API_KEY=secret-key --name ollama docker.io/ollama/ollama
 podman exec -it ollama ollama pull qwen3:8b
+podman logs --follow ollama
