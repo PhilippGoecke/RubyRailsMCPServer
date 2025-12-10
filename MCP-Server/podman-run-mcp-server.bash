be02a61912c32814cd16d4f3357364147c01441b
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-12-10 13:09:06 +0100

    Add RAILS_LOG_LEVEL environment variable to podman run

--- MCP-Server/podman-run-mcp-server.bash
+++ MCP-Server/podman-run-mcp-server.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm --file Containerfile --tag mcpserver:demo .
-podman run --interactive --tty --publish 3000:3000 mcpserver:demo
+podman run --interactive --tty --publish 3000:3000 --env RAILS_LOG_LEVEL=debug mcpserver:demo
 echo "browse http://localhost:3000/mcp"
